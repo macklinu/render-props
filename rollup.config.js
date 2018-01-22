f8aa11deb5390b55ebcbb7b3af27ad287488fcf6
Macklin Underdown <macklinu@users.noreply.github.com>
2018-01-22 14:05:26 -0500

    build(rollup): set up rollup for bundling library

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
+import filesize from 'rollup-plugin-filesize'
+import sourceMaps from 'rollup-plugin-sourcemaps'
+import uglify from 'rollup-plugin-uglify'
+
+const createOutput = options => ({
+  name: 'renderProps',
+  globals: {
+    react: 'React',
+  },
+  ...options,
+})
+
+export default {
+  input: 'src/index.js',
+  output: [
+    createOutput({ file: 'dist/index.js', format: 'cjs' }),
+    createOutput({ file: 'dist/index.es.js', format: 'es' }),
+    createOutput({ file: 'dist/index.umd.js', format: 'umd' }),
+  ],
+  sourceMap: true,
+  external: ['react'],
+  plugins: [resolve(), commonjs(), babel(), sourceMaps(), filesize(), uglify()],
+}
