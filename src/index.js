6f865f0a015d975b87dc8fe44ff215805b3ea2fd
Macklin Underdown <macklinu@users.noreply.github.com>
2018-01-22 13:12:00 -0500

    feat: create renderProps() function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-// TODO
+import React from 'react'
+
+export default function renderProps(props, newProps) {
+  if (!isObject(props)) {
+    throw new Error('Must supply truthy component props')
+  }
+  if (!isObject(newProps)) {
+    throw new Error(
+      'Must supply new props object to forward to render component'
+    )
+  }
+
+  if (props.component) {
+    return React.createElement(props.component, newProps)
+  }
+  if (props.render) {
+    return props.render(newProps)
+  }
+  if (props.children) {
+    if (typeof props.children === 'function') {
+      return props.children(newProps)
+    }
+    if (React.Children.count(props.children) > 0) {
+      return React.Children.only(props.children)
+    }
+    return null
+  }
+  return null
+}
+
+function isObject(obj) {
+  return obj !== null && !Array.isArray(obj) && typeof obj === 'object'
+}
--- src/index.test.js
+++ src/index.test.js
@@ -1,1 +1,68 @@
-test('todo')
+import React from 'react'
+import renderer from 'react-test-renderer'
+import renderProps from '.'
+
+const Counter = props => renderProps(props, { count: 1 })
+
+test('accepts component prop', () => {
+  const Component = ({ count }) => <div>{count}</div>
+
+  expect(render(<Counter component={Component} />)).toMatchSnapshot()
+})
+
+test('accepts render prop', () => {
+  expect(
+    render(<Counter render={({ count }) => <div>{count}</div>} />)
+  ).toMatchSnapshot()
+})
+
+test('accepts children function prop', () => {
+  expect(
+    render(<Counter>{({ count }) => <div>{count}</div>}</Counter>)
+  ).toMatchSnapshot()
+})
+
+test('renders component prop above all other props passed to it', () => {
+  const Component = ({ count }) => <div>{count}</div>
+  const MockComponent = jest.fn(props => <Component {...props} />)
+  const RenderComponent = jest.fn(props => <Component {...props} />)
+  const ChildrenComponent = jest.fn(props => <Component {...props} />)
+
+  render(
+    <Counter component={MockComponent} render={RenderComponent}>
+      {ChildrenComponent}
+    </Counter>
+  )
+
+  expect(MockComponent).toHaveBeenCalled()
+  expect(RenderComponent).not.toHaveBeenCalled()
+  expect(ChildrenComponent).not.toHaveBeenCalled()
+})
+
+test('throws if no component props are passed', () => {
+  const InvalidComponent = _props => renderProps(null, { any: 'any' })
+
+  expect(() => render(<InvalidComponent />)).toThrowErrorMatchingSnapshot()
+})
+
+test('throws if no new props are passed', () => {
+  const InvalidComponent = props => renderProps(props)
+
+  expect(() => render(<InvalidComponent />)).toThrowErrorMatchingSnapshot()
+})
+
+test('throws if component props is truthy but not an object', () => {
+  const InvalidComponent = _props => renderProps('nope', { any: 'any' })
+
+  expect(() => render(<InvalidComponent />)).toThrowErrorMatchingSnapshot()
+})
+
+test('throws if new props are truthy but not an object', () => {
+  const InvalidComponent = props => renderProps(props, 'nope')
+
+  expect(() => render(<InvalidComponent />)).toThrowErrorMatchingSnapshot()
+})
+
+function render(element) {
+  return renderer.create(element).toJSON()
+}
